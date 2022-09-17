5ceb8db7c791b499f0eeb01e44db4e9b5bb169d2
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-09-17 21:26:25 +0800

    add page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import LinkTree from './components/LinkTree'
+import { Helmet } from 'react-helmet'
 import './App.css'
 
 const App = (): JSX.Element => {
   return (
     <>
+      <Helmet>
+        <title>Teo Wei Shen</title>
+      </Helmet>
       <Header />
       <LinkTree />
     </>
